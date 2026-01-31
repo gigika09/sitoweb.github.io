0220078fbb2ade21dd3c906edd9e10d50d0556b9
gigika09 <angy.frijio09@gmail.com>
2026-01-31 21:12:58 +0100

    Update service worker cache name and fetch logic
    Updated cache name and modified service worker to handle fetch requests more efficiently.

--- sw.js
+++ sw.js
@@ -1,20 +1,22 @@
-const CACHE_NAME = "decoder118-v3";
+const CACHE_NAME = "decoder118-v4";
 
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./sw.js"
-];
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+self.addEventListener("activate", event => {
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.open(CACHE_NAME).then(cache =>
+      cache.match(event.request).then(response => {
+        return response || fetch(event.request).then(networkResponse => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      })
+    )
   );
 });
